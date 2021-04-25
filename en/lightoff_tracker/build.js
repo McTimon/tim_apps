2cd2da9bd32225cf89b6cabe4e5469275e793656
periwinkle <mctimon3d@gmail.com>
2021-04-25 10:40:16 +0000

    lightoff live initial commit

---
+++ en/lightoff_tracker/build.js
@@ -0,0 +1,1 @@
+(()=>{console.log("hello to the world");var t=L.map("mapid").setView([5.7,-.2],13);L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(t)})();
